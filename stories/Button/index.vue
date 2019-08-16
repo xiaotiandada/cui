1e3d8147782313fa05b77e51c492e90c08bdbc8b
xiaotiandada <952822399@qq.com>
2019-08-16 18:27:10 +0800

    Button

---
+++ stories/Button/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <button
+    :disabled="disabled"
+    class="c-button"
+    :class="[type ? 'c-button-' + type : '']"
+    :style="style"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "CButton",
+  props: {
+    // 按钮类型
+    type: {
+      type: String,
+      default: "default"
+    },
+    // 自定义样式
+    style: {
+      type: Object,
+      default: () => {}
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style>
+@import url(../common/var.css);
+.c-button {
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  -webkit-appearance: none;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  margin: 0;
+  transition: 0.1s;
+  font-weight: 500;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+}
+.c-button[disabled] {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+.c-button-default {
+  background: var(--color-white);
+  color: var(--color-text-regular);
+  border: 1px solid var(--border-color-base);
+}
+.c-button-primary {
+  color: var(--color-white);
+  background-color: var(--color-primary);
+  border-color: var(--color-primary);
+}
+.c-button-success {
+  background-color: var(--color-success);
+  color: var(--color-white);
+  border-color: var(--color-success);
+}
+.c-button-info {
+  background-color: var(--color-info);
+  color: var(--color-white);
+  border-color: var(--color-info);
+}
+.c-button-warning {
+  background-color: var(--color-warning);
+  color: var(--color-white);
+  border-color: var(--color-warning);
+}
+.c-button-danger {
+  background-color: var(--color-danger);
+  color: var(--color-white);
+  border-color: var(--color-danger);
+}
+</style>
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,36 +1,92 @@
 /* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
 
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/vue";
+// import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import MyButton from './MyButton';
-import Welcome from './Welcome';
+// import MyButton from "./MyButton";
+import Welcome from "./Welcome";
+import CButton from "./Button/index.vue";
 
-storiesOf('Welcome', module).add('to Storybook', () => ({
+storiesOf("Welcome", module).add("to Storybook", () => ({
   components: { Welcome },
   template: '<welcome :showApp="action" />',
-  methods: { action: linkTo('Button') },
+  methods: { action: linkTo("Button") }
 }));
 
-storiesOf('Button', module)
-  .add('with text', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">Hello Button</my-button>',
-    methods: { action: action('clicked') },
-  }))
-  .add('with JSX', () => ({
-    components: { MyButton },
-    // eslint-disable-next-line no-unused-vars
-    render(h) {
-      return <my-button onClick={this.action}>With JSX</my-button>;
+// storiesOf("Button", module)
+//   .add("with text", () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">Hello Button</my-button>',
+//     methods: { action: action("clicked") }
+//   }))
+//   .add("with JSX", () => ({
+//     components: { MyButton },
+//     // eslint-disable-next-line no-unused-vars
+//     render(h) {
+//       return <my-button onClick={this.action}>With JSX</my-button>;
+//     },
+//     methods: { action: linkTo("clicked") }
+//   }))
+//   .add("with some emoji", () => ({
+//     components: { MyButton },
+//     template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+//     methods: { action: action("clicked") }
+//   }));
+
+storiesOf("Button", module)
+  .add("button 按钮", () => ({
+    components: { CButton },
+    data() {
+      return {
+        style: {
+          width: "40px",
+          height: "40px",
+          padding: 0,
+          borderRadius: "50%"
+        },
+        style1: {
+          borderRadius: "20px"
+        }
+      };
     },
-    methods: { action: linkTo('clicked') },
+    template: `
+    <div>
+      <p>基础用法</p>
+      <c-button>默认按钮</c-button>
+      <c-button type="primary">主要按钮</c-button>
+      <c-button type="success">成功按钮</c-button>
+      <c-button type="info">信息按钮</c-button>
+      <c-button type="warning">警告按钮</c-button>
+      <c-button type="danger">危险按钮</c-button>
+      <p>禁用状态</p>
+      <c-button disabled="true">默认按钮</c-button>
+      <c-button type="primary" disabled="true">主要按钮</c-button>
+      <c-button type="success" disabled="true">成功按钮</c-button>
+      <c-button type="info" disabled="true">信息按钮</c-button>
+      <c-button type="warning" disabled="true">警告按钮</c-button>
+      <c-button type="danger" disabled="true">危险按钮</c-button>
+      <p>扩展用法</p>
+      <p>使用 style(如果喜欢css in js) || 使用class(尽情发挥css的才能)</p>
+      <c-button :style="style">1</c-button>
+      <c-button type="primary" :style="style">2</c-button>
+      <c-button type="success" :style="style">3</c-button>
+      <c-button type="info" :style="style">4</c-button>
+      <c-button type="warning" :style="style">5</c-button>
+      <c-button type="danger" :style="style">6</c-button>
+      <br>
+      <br>
+      <c-button :style="style1">默认按钮</c-button>
+      <c-button type="primary" :style="style1">主要按钮</c-button>
+      <c-button type="success" :style="style1">成功按钮</c-button>
+      <c-button type="info" :style="style1">信息按钮</c-button>
+      <c-button type="warning" :style="style1">警告按钮</c-button>
+      <c-button type="danger" :style="style1">危险按钮</c-button>
+    </div>
+  `
   }))
-  .add('with some emoji', () => ({
-    components: { MyButton },
-    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-    methods: { action: action('clicked') },
+  .add("自行YY脑洞 大胆PR", () => ({
+    template: `<p>自行YY脑洞 大胆PR</p>`
   }));
 
 /* eslint-enable react/react-in-jsx-scope */
